bf20232d6394fa5828751da57f50186750b62c49
NatashaSolntseva <84683371+NatashaSolntseva@users.noreply.github.com>
2023-04-21 18:43:06 +0300

    backend added

---
+++ backend/src/users/users.controller.ts
@@ -0,0 +1,84 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+  Req,
+  HttpCode,
+  BadRequestException,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { JwtGuard } from 'src/auth/jwt.guard';
+import { User } from './entities/user.entity';
+import { FindUserDto } from './dto/find-user.dto';
+import { PublicUserDto } from './dto/public-user.dto';
+
+
+@UseGuards(JwtGuard)
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  // for signup
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.usersService.findAll();
+  }
+
+  // Get Current User  
+  @Get('me')
+  findCurrentUser(@Req() req):Promise<User> {
+    //console.log('req.user', req.user); 
+    return this.usersService.findOne(req.user.id);
+  }
+
+  // Update User Info
+  @Patch('me')
+  async update(@Req() req, @Body() updateUserDto: UpdateUserDto): Promise<User> {
+    await this.usersService.updateUserData(req.user.id, updateUserDto);
+    return this.usersService.findOne(req.user.id);
+  }
+  
+  @Get('me/wishes')
+  async getCurrentUserWishes(@Req() req) {
+    const user = await this.usersService.findOne(req.user.id);
+    const userWishes = await this.usersService.getUserWishes(user.username);
+    return userWishes;
+  }
+
+  // Find user by Username  
+  @Get(':username')
+  findOne(@Param('username') username: string) {
+    return this.usersService.findByUserNamePublic(username);
+  }
+  
+  @Get(':username/wishes')
+  getUsersWishes(@Param('username') username: string) {
+    return this.usersService.getUserWishes(username);    
+  }
+  
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    if (isNaN(+id)) {
+      return new BadRequestException('Переданный id не явялется числом');
+    }
+    return this.usersService.remove(+id);
+  }
+// Find User by email or username, unguarded
+  @Post('find')
+  @HttpCode(200)
+  async findMany(@Body() findUsersDto: FindUserDto): Promise<PublicUserDto[]> {
+    return this.usersService.findMany(findUsersDto);
+  }
+}
---
+++ backend/src/wishlists/wishlists.service.ts
@@ -0,0 +1,88 @@
+import { ForbiddenException, Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { PublicUserDto } from 'src/users/dto/public-user.dto';
+import { User } from 'src/users/entities/user.entity';
+import { UsersService } from 'src/users/users.service';
+import { WishesService } from 'src/wishes/wishes.service';
+import { In, Repository } from 'typeorm';
+import { CreateWishlistDto } from './dto/create-wishlist.dto';
+import { UpdateWishlistDto } from './dto/update-wishlist.dto';
+import { Wishlist } from './entities/wishlist.entity';
+
+@Injectable()
+export class WishlistsService {
+  constructor(
+    @InjectRepository(Wishlist)
+    private readonly wishListRepository: Repository<Wishlist>,
+    private readonly wishesService: WishesService,
+    private readonly userService: UsersService    
+  ) {}
+
+  async create(createWishlistDto: CreateWishlistDto, user: User): Promise<Wishlist> {    
+    const wishes = await this.wishesService.find({
+      where: { id: In(createWishlistDto.itemsId || [])},
+    });
+
+    const wishList = this.wishListRepository.create({
+      ...createWishlistDto,
+      owner: PublicUserDto.getFromUser(user),
+      items: wishes            
+    });
+        
+    return this.wishListRepository.save(wishList);
+  }
+
+  findAll(): Promise<Wishlist[]> {
+    return this.wishListRepository.find({relations: 
+      ['items', 'owner']
+    });
+  }
+
+  findOne(id: number): Promise<Wishlist> {
+    return this.wishListRepository.findOne({
+      where: { id },
+      relations: ['items', 'owner']
+    })
+  }
+
+  async updateOne(idWishlist: number, userId: number, updateWishlistDto: UpdateWishlistDto) {
+    const updatingWishList = await this.findOne(idWishlist);
+
+    if (!updatingWishList) {
+      throw new NotFoundException();
+    }
+
+    const currentUser = await this.userService.findOne(userId);
+
+    if (updatingWishList.owner.id !== currentUser.id) {
+      throw new ForbiddenException('Можно редактировать только свой вишлист')    
+    }
+
+    const wishes = await this.wishesService.find({
+      where: { id: In(updateWishlistDto.itemsId || [])},
+    });
+
+    const updatedWishlist = await {
+      ...updateWishlistDto,
+      id: idWishlist,
+      updatedAt: new Date(),      
+      items: wishes,
+    };
+
+    await delete updatedWishlist.itemsId;   
+  
+    return this.wishListRepository.save(updatedWishlist);
+  }
+  
+  async removeOne(wishListId: number): Promise<Wishlist> {
+    const wishListToDelete = await this.findOne(wishListId);
+
+    if (!wishListToDelete) {
+      throw new NotFoundException();
+    }    
+
+    await this.wishListRepository.delete(wishListId);
+
+    return wishListToDelete;
+  }
+}
